7c810753951b85442c368b6f507c561d0b7bccd1
RanHao000 <54371206+RanHao000@users.noreply.github.com>
2022-04-17 21:26:43 -0500

    ignore issue

--- NotificationSystem/src/com/company/PushNotification.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class PushNotification implements sendNotification{
-    @Override
-    public void sendNotification() {
-        System.out.println("You will get notification via Push in the future");
-    }
-}
