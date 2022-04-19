6ee5b30f40d0d0dad971a93bfa7d33a10423e06a
zhichengzhaoxh <zhichengzhaoxh@gmail.com>
2022-04-18 22:44:14 -0700

    coding hw2

---
+++ MessageCenter/src/main/java/User.java
@@ -0,0 +1,25 @@
+public class User {
+    protected String username;
+    private String email;
+    private String phoneNo;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+}
