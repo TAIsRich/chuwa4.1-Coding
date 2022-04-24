9151ae2925f27da12de3b51fc90be1b161699f9f
shirley <shielry.shen888@gmail.com>
2022-04-24 14:39:43 -0700

    springboot hw1

---
+++ springboot-demo/main/java/com/chuwa/SpringbootDemoApplication.java
@@ -0,0 +1,19 @@
+package com.chuwa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+
+
+
+}
