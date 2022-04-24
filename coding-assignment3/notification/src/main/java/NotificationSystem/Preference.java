bb823b097b88bab9063455b5a96edad8c6863501
huan wang <huanwang@huans-MacBook-Air.local>
2022-04-24 07:33:02 -0700

    spring boot RESTful api

---
+++ coding-assignment3/notification/src/main/java/NotificationSystem/Preference.java
@@ -0,0 +1,5 @@
+package NotificationSystem;
+
+public enum Preference {
+    Email,Sms
+}
---
+++ coding-assignment3/notification/src/main/java/com/springboot/app/User.java
@@ -0,0 +1,28 @@
+package com.springboot.app;
+
+public class User {
+
+    private String userName;
+    private Integer userId;
+
+    public User(String userName, Integer userId) {
+        this.userName = userName;
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+}
