4138025bcf0193c8b3007ef2585ba34d21d81920
bcpy1314 <panyueabc@gmail.com>
2022-05-03 09:57:49 -0700

    updated User Class

--- Week4-Assignment/userRegistration/src/main/java/com/example/userregistration/entity/User.java
+++ Week4-Assignment/userRegistration/src/main/java/com/example/userregistration/entity/User.java
@@ -31,12 +31,10 @@ public class User {
     @Column(nullable = false)
     private String username;
 
-    @Column
-    @JsonIgnore
+    @Column(nullable = false)
     private String password;
 
     @Column
-    @JsonIgnore
     private boolean enabled;
 
 
